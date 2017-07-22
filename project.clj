abf45bef31954432b9bf94d5cd9e59c15beb2729
agaric <agaric@protonmail.com>
2017-07-22 21:20:12 +0900

    version update

--- project.clj
+++ project.clj
@@ -1,26 +1,25 @@
 (defproject collar "0.1.0"
   :description "alocy.be base"
-  :url "http://alocy.be"
+  :url "https://alocy.be"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.671"]
-                 [compojure "1.5.1"]
+                 [compojure "1.6.0"]
                  [yogthos/config "0.8"]
                  [com.datomic/datomic-free "0.9.5561.50"]
                  [expectations "2.2.0-beta1"]
                  [hiccup "1.0.5"]
                  [markdown-clj "0.9.99"]
-                 [ring/ring-defaults "0.2.1"]
-                 [ring/ring-jetty-adapter "1.5.0"]
-                 [clj-time "0.13.0"]]
+                 [ring/ring-defaults "0.3.0"]
+                 [ring/ring-jetty-adapter "1.6.2"]
+                 [clj-time "0.14.0"]]
   :plugins [[lein-autoexpect "1.9.0"]
             [lein-figwheel "0.5.11"]
-            [lein-ring "0.9.7"]]
+            [lein-ring "0.12.0"]]
   :ring {:handler collar.core/app}
   :main collar.core
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]
+  {:dev {:dependencies [[ring/ring-mock "0.3.1"]]
          :resource-paths ["config/development"]}
    :next {:resource-paths ["config/next"]}
    :master {:resource-paths ["config/master"]}
