d1df34c0f37d97ad11e1a34b184d11aa3202e275
agaric <agaric@protonmail.com>
2017-08-14 02:15:37 +0900

    add google tracking tag

--- src/collar/piece.clj
+++ src/collar/piece.clj
@@ -7,6 +7,7 @@
    [:title (str title " - alocy.be")]
    [:meta {:name "viewport"
            :content "width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=0"}]
+   [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-104505539-1', 'auto');ga('send', 'pageview');"]
    [:link {:href "https://fonts.googleapis.com/css?family=Source+Code+Pro|Source+Sans+Pro"
            :rel "stylesheet"}]
    (h/include-css "/css/styles.css")])
