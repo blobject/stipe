6344a2a17d40999b645bb89f53918ec20864768b
agaric <agaric@protonmail.com>
2017-12-28 18:55:50 +0100

    add table sorting

--- resources/scripts/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd $HOME/src/collar/db/
-rsync *.md alocy.be:/srv/alocy.be/db/ -tuv
-cd $HOME/src/collar/ignore/img/
-rsync *g alocy.be:/srv/alocy.be/ignore/img/ -tuv
--- src/collar/piece.clj
+++ src/collar/piece.clj
@@ -23,7 +23,8 @@
    [:link {:href "https://fonts.googleapis.com/css?family=Source+Code+Pro|Source+Sans+Pro"
            :rel "stylesheet"}]
    (if u/is-master?
-     [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-104505539-1', 'auto');ga('send', 'pageview');"])])
+     [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-104505539-1', 'auto');ga('send', 'pageview');"])
+   [:script {:src "/js/tablesort.min.js"}]])
 
 (defn nav [title]
   (let [here (fn [where] (if (= where title) "here"))]
