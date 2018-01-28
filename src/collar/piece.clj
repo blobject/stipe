6250522727cdc0d1a3f6cf628e5c01e634db4f48
agaric <agaric@protonmail.com>
2018-01-28 22:29:40 +0100

    update

--- src/collar/piece.clj
+++ src/collar/piece.clj
@@ -15,15 +15,16 @@
 
 (defn head [title]
   [:head
+   (if u/is-master?
+     [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-104505539-1', 'auto');ga('send', 'pageview');"])
    [:title (str title " - " u/site-name)]
    [:meta {:name "viewport"
            :content "width=device-width, initial-scale=1"}]
-   [:link {:href "/favicon.ico" :rel "icon"}]
+   [:link {:rel "icon"
+           :href "/favicon.ico"}]
    (h/include-css "/css/style.css")
-   [:link {:href "https://fonts.googleapis.com/css?family=Source+Code+Pro|Source+Sans+Pro"
-           :rel "stylesheet"}]
-   (if u/is-master?
-     [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-104505539-1', 'auto');ga('send', 'pageview');"])
+   [:link {:rel "stylesheet"
+           :href "https://fonts.googleapis.com/css?family=Source+Code+Pro|Source+Sans+Pro"}]
    [:script {:src "/js/tablesort.min.js"}]])
 
 (defn nav [title]
